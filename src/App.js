ea0113dce77b7e9df0b2e07ba8856992c549ce49
Hershey Lande <hersheylande@gmail.com>
2019-07-01 14:25:34 -0700

    add new progress

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import logo from './logo.png'
 import arrow from './arrow.png'
 import TextInput from './TextInput'
 
-function App() {
-  return (
-    <div className="App">
-      <header className = "header"> 
-        <img src= {logo} className = "logo" />  
-        Chatter 
+class App extends React.Component {
+
+  state ={
+    messages:[]
+  }
+
+  sendMessage = (m) => {
+    var messages =[...this.state.messages, m]
+    this.setState({messages})
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <img src={logo} className="logo" />
+          Chatter
       </header>
-      <TextInput>
-      <button>
-        <img src= {arrow} className = "arrow" />
-      </button>
-      </TextInput>
-    </div>
-  );
+        <TextInput sendMessage={this.sendMessage }/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import arrow from './arrow.png'
+import { FiSend, FiArrowRight } from "react-icons/fi";
 
-class TextInput extends React.Component{
-    render(){
-        return (<div className = "text-input">
-            <input />
-            <button>
-            <img src= {arrow} className = "arrow" />
+class TextInput extends React.Component {
+
+    state = {
+        text: ""
+    }
+
+    send = () => {
+        this.props.sendMessage(this.state.text);
+        this.setState({ text: "" })
+    }
+
+    keyPress = (e) => {
+        if(e.key == 'Enter'){
+            this.send()
+        }
+    }
+    render() {
+        return (<div className="text-input">
+            <input value={this.state.text}
+                placeholder="Write your message here..."
+                onChange={e => this.setState({ text: e.target.value })}
+                onKeyPress={this.keyPress}
+            />
+            <button disabled={!this.state.text}
+                onClick={this.send}>
+                <FiArrowRight style={{ height: 15, width: 15 }} />
             </button>
-            </div>)
+        </div>)
     }
 }
 
