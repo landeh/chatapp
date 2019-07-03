0e8b0ad366149862d8be57d3d59c57835d8a6596
Hershey Lande <hersheylande@gmail.com>
2019-07-03 12:39:23 -0700

    add more changes

--- src/App.js
+++ src/App.js
@@ -7,21 +7,36 @@ import TextInput from './TextInput'
 class App extends React.Component {
 
   state ={
-    messages:[]
+    messages:[],
+    name:'',
+    editName: ''
   }
 
-  sendMessage = (m) => {
-    var messages =[...this.state.messages, m]
-    this.setState({messages})
+  gotMessage = (m) => {
+    var newMessagesArray =[...this.state.messages, m]
+    this.setState({messages: newMessagesArray})
   }
+
   render() {
+    var {messages} = this.state
     return (
       <div className="App">
         <header className="header">
           <img src={logo} className="logo" />
           Chatter
+        <namePicker/>  
       </header>
-        <TextInput sendMessage={this.sendMessage }/>
+      <main className = "messages">
+        {messages.map((m,i) => {
+          return <div key= {i} className= "bubble-wrap"> 
+            <div className ="bubble"> 
+              <span> {m}</span>
+            </div>
+          </div>
+        })}
+
+      </main>
+        <TextInput sendMessage={this.gotMessage }/>
       </div>
     );
   }
